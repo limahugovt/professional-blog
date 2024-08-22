30c4cd3205df539d8638d22b4f7d833e78f6cf1a
limahugovt <lima.hugo@escolar.ifrn.edu.br>
2024-08-22 18:58:51 -0300

    chore: Update index.html template to display all posts and improve styling

--- courses/views.py
+++ courses/views.py
@@ -4,6 +4,7 @@ from .models import Post
 def home(request):
     context = {
         'posts_relevant': Post.objects.filter(relevant=True).order_by('-publish')[:1],
-        'posts': Post.objects.filter(status='published').order_by('-publish')[:2]
+        'posts_recents': Post.objects.filter(status='published').order_by('-publish')[:2],
+        'all_posts': Post.objects.filter(status='published').order_by('-publish')[:6]
     }
     return render(request, 'index.html', context)
