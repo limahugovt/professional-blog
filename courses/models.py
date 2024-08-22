fedd9593ffba59b639c23917ab9ac62cca601b9c
Victor Hugo <lima.hugo@escolar.ifrn.edu.br>
2024-08-22 09:35:04 -0300

    feat: Register models in admin.py and create initial migrations for courses app

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=100, unique=True)
+  description = models.TextField(blank=True)
+
+  def __str__(self):
+    return self.name
+
+class Post(models.Model):
+  STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+  )
+  title = models.CharField(max_length=250)
+  slug = models.SlugField(max_length=250, unique_for_date='publish')
+  author = models.ForeignKey(User, on_delete=models.CASCADE,)
+  content = models.TextField()
+  publish = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+  class Meta:
+    ordering = ('-publish',)
+
+  def __str__(self):
+    return self.title
+
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+  name = models.CharField(max_length=80)
+  email = models.EmailField()
+  body = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+  approved = models.BooleanField(default=False)
+
+  class Meta:
+    ordering = ('created',)
+
+  def __str__(self):
+    return f'Comment by {self.name} on {self.post}'
