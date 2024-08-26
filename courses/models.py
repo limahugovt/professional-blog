a1335a322ea60518818c72fb8e9e4036ecfd987a
Victor Hugo <lima.hugo@escolar.ifrn.edu.br>
2024-08-26 10:53:24 -0300

    chore: Add image field to Category model and update models.py and migrations

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
   name = models.CharField(max_length=100, unique=True)
   description = models.TextField(blank=True)
+  image = models.ImageField(upload_to='categories/', blank=False)
 
   def __str__(self):
     return self.name
--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 
 def home(request):
     context = {
         'posts_relevant': Post.objects.filter(relevant=True).order_by('-publish')[:1],
         'posts_recents': Post.objects.filter(status='published').order_by('-publish')[:2],
-        'all_posts': Post.objects.filter(status='published').order_by('-publish')[:6]
+        'all_posts': Post.objects.filter(status='published').order_by('-publish')[:6],
+        'categories': Category.objects.all()[:3]
     }
     return render(request, 'index.html', context)
