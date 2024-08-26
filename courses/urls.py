8831a61790986c8f22e92135cadaf8f5005703c1
Victor Hugo <lima.hugo@escolar.ifrn.edu.br>
2024-08-26 11:13:12 -0300

    chore: Update index.html template to display all posts and improve styling

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, detail_view
 
 urlpatterns = [
     path('', home, name='home'),
+    path('detail/<int:id>/', detail_view , name='detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 
 def home(request):
     context = {
         'posts_relevant': Post.objects.filter(relevant=True).order_by('-publish')[:1],
-        'posts_recents': Post.objects.filter(status='published').order_by('-publish')[:2],
-        'all_posts': Post.objects.filter(status='published').order_by('-publish')[:6],
+        'posts_recents': Post.objects.filter(status='published').order_by('-publish')[:7],
         'categories': Category.objects.all()[:3]
     }
     return render(request, 'index.html', context)
+
+def detail_view(request, id):
+    post = get_object_or_404(Post, id=id)
+    context = {'post': post}
+    return render(request, 'pagePost.html', context)
