b30337912b920b9c9adcb6da87697b2cc0f74d4b
limahugovt <lima.hugo@escolar.ifrn.edu.br>
2024-08-27 11:02:01 -0300

    implement

--- courses/views.py
+++ courses/views.py
@@ -5,13 +5,17 @@ def home(request):
     context = {
         'posts_relevant': Post.objects.filter(relevant=True).order_by('-publish')[:1],
         'posts_recents': Post.objects.filter(status='published').order_by('-publish')[:7],
+        'posts_featured': Post.objects.filter(status='published').order_by('-publish')[:2],
         'categories': Category.objects.all()[:3]
     }
     return render(request, 'index.html', context)
 
 def detail_view(request, id):
     post = get_object_or_404(Post, id=id)
-    context = {'post': post}
+    context = {
+        'post': post,
+        'posts_recents': Post.objects.filter(status='published').order_by('-publish')[:7],
+               }
     return render(request, 'pagePost.html', context)
 
 def detail_view_category(request, id):
